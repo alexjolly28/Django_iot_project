b704f9f288cf41645d38d30e779cab668a289591
alexjolly28 <alexjolly28@gmail.com>
2020-02-27 22:16:29 +0530

    re arrange

--- iot/views.py
+++ iot/views.py
@@ -10,7 +10,6 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
 
-
 class IotView(generics.GenericAPIView, mixins.ListModelMixin):
     queryset = IotModel.objects.all()
     serializer_class = IotModelSerializer
