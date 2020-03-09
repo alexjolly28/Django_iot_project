cd247760c9fed0d9733c968022a245300d0aa87b
alexjolly28 <alexjolly28@gmail.com>
2020-03-09 23:47:15 +0530

    phase 1 completed

--- iot/views.py
+++ iot/views.py
@@ -10,7 +10,7 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 
 
-class IotView(generics.GenericAPIView, mixins.ListModelMixin):
+class IotView(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
     queryset = IotModel.objects.all()
     serializer_class = IotModelSerializer
 
@@ -19,43 +19,21 @@ class IotView(generics.GenericAPIView, mixins.ListModelMixin):
 
     def post(self, request):
         device_name = request.data['device_name']
-        # status = request.data['status']
+        status = request.data['status']
         queryset = IotModel.objects.get(device_name=device_name)
         serializer = IotModelSerializer(queryset)
-        # json = JSONRenderer().render(serializer.data)
-        # print(json)
-        # data = json['']
-        print(serializer.data['status'])
+        # id = serializer.data['id']
+        current_status = serializer.data['status']
+        if current_status == status:
+            print("it is alredy ", status)
+            return Response(serializer.data)
 
-        # print(serializer.data['status'])
+        else:
+            print("turning", status)
+            serializer = IotModelSerializer(queryset, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        # headers = self.get_success_headers(serializer.data)
-        return Response(serializer.data)
 
-# class IotView(APIView):
-#     """
-#     List all snippets, or create a new snippet.
-#     """
-#
-#     def get_object(self):
-#         try:
-#             return IotModel.objects.all()
-#         except IotModel.DoesNotExist:
-#             raise status.HTTP_404_NOT_FOUND
-#
-#     def get(self, request):
-#         iot = IotModel.objects.all()
-#         serializer = IotModelSerializer(iot, many=True)
-#         return Response(data=serializer.data, status=status.HTTP_200_OK)
-#
-#     def post(self, request, format=None):
-#         # print(request.data)
-#         # request.data.get("sheet")
-#         queryset = IotModel.objects.get(id=2)
-#
-#         serializer = IotModelSerializer(queryset)
-#         if serializer.is_valid():
-#             print(serializer.data.get("device_name"))
-#             #     serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
