448ab090bb5c580304caa463bdfa777f12e2d423
alexjolly28 <alexjolly28@gmail.com>
2020-03-14 01:04:01 +0530

    mqtt added

--- iot/mqtt.py
+++ iot/mqtt.py
@@ -1,6 +1,45 @@
-import paho.mqtt.client as mqtt  # import the client1
+import paho.mqtt.client as mqtt
+import time
 
-broker_address = "localhost"
-client = mqtt.Client("P1")  # create new instance
-client.connect(broker_address)  # connect to broker
-client.publish("test", "OFF")  # publish
+broker_url = "localhost"
+broker_port = 1883
+
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected With Result Code "(rc))
+
+
+def on_message_from_kitchen(client, userdata, message):
+    print("Message Recieved from Kitchen: " + message.payload.decode())
+
+
+def on_message_from_bedroom(client, userdata, message):
+    print("Message Recieved from Bedroom: " + message.payload.decode())
+
+
+def on_message(client, userdata, message):
+    print("Message Recieved from Others: " + message.payload.decode())
+
+
+def on_publish(topic, payload):
+    client.publish(topic=topic, payload=payload, qos=1, retain=False)
+
+
+client = mqtt.Client()
+client.on_connect = on_connect
+# To Process Every Other Message
+client.on_message = on_message
+client.connect(broker_url, broker_port)
+
+client.subscribe("test", qos=1)
+client.subscribe("test1", qos=1)
+client.subscribe("test2", qos=1)
+client.message_callback_add("test1", on_message_from_kitchen)
+client.message_callback_add("test2", on_message_from_bedroom)
+client.loop_start()
+# time.sleep(5)
+on_publish(topic="test1", payload="pub")
+time.sleep(5)
+
+client.loop_stop()
+# client.loop_forever()
