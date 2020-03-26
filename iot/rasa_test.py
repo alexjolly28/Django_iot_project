7210d9c898cd7c7cc3017faf4ee0fde99ad8ba91
alexjolly28 <alexjolly28@gmail.com>
2020-03-26 14:17:57 +0530

    rasa api added

---
+++ iot/rasa_test.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+a = {}
+url = 'http://127.0.0.1:5005/model/parse'
+
+api_url = 'http://127.0.0.1:8000/iot/'
+
+
+def main(text):
+    x = requests.post(url, json=text)
+
+    data = x.text
+    json_data = json.loads(data)
+    entities = json_data["entities"]
+    intents = json_data["intent"]
+    acc = intents["confidence"]
+    if acc >= .9:
+        device = intents["name"]
+        a["device_name"] = device
+        for entity in entities:
+            if entity["entity"] == "action":
+                a["status"] = entity["value"]
+            if entity["entity"] == "value":
+                a["colour"] = entity["value"]
+
+    json_a = json.dumps(a)
+    return (json_a)
+
+
+if __name__ == '__main__':
+    text = {"text": "turn  on green light  "}
+
+    data_json = main(text)
+    print(data_json)
+    # test_json = {"device_name": "fan", "status": "off"}
+    resp = requests.post(api_url, json=data_json)
+    print(resp)
--- iot/views.py
+++ iot/views.py
@@ -1,14 +1,10 @@
 from iot.models import IotModel
 from iot.serialzers import IotModelSerializer
-from django.http import Http404
-from rest_framework.views import APIView
 from rest_framework.response import Response
-# from rest_framework import status
 from rest_framework import generics
 from rest_framework import mixins
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from .mqtt import on_publish
+import json
 
 
 class IotView(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin):
@@ -19,21 +15,25 @@ class IotView(generics.GenericAPIView, mixins.ListModelMixin, mixins.UpdateModel
         return self.list(request)
 
     def post(self, request):
-        device_name = request.data['device_name']
-        status = request.data['status']
+        json_data = json.loads(request.data)
+
+        device_name = json_data['device_name']
+        status = json_data['status']
         queryset = IotModel.objects.get(device_name=device_name)
         serializer = IotModelSerializer(queryset)
         # id = serializer.data['id']
         current_status = serializer.data['status']
         if current_status == status:
-            print("it is alredy ", status)
+            print(device_name, "is alredy ", status)
             return Response(serializer.data)
 
         else:
             print("turning", status)
             on_publish(topic=device_name, payload=status)
-            serializer = IotModelSerializer(queryset, data=request.data)
+            serializer = IotModelSerializer(queryset, data=json_data)
             if serializer.is_valid():
                 serializer.save()
                 return Response(serializer.data)
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            print("ok")
+            return Response(serializer.errors)
+
